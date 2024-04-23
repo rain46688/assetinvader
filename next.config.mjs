48002bf9f970f011950be8766eab93c0ca2f5cba
rain46688 <rain46688@gmail.com>
2024-04-23 23:01:43 +0900

    240423-서버 접속 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    devServer: {
+        // 개발 서버를 모든 IP 주소에 바인딩
+        // 이렇게 하면 외부에서도 접속할 수 있게 됩니다.
+        host: '0.0.0.0'
+    }
 };
 
 export default nextConfig;
