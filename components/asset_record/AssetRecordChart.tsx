b2325cf09daae58d8728148dfadfb7aa6ef7cc74
cmw1017 <cmw1017@naver.com>
2024-05-11 22:27:32 +0900

    240511-자산 기록 테스트 완료

--- components/asset_record/AssetRecordChart.tsx
+++ components/asset_record/AssetRecordChart.tsx
@@ -77,8 +77,10 @@ export default function AssetRecordChart() {
             // 데이터 저장 및 변환
             list.forEach(list_data => {
                 const [itemYear, itemMonth] = formatDateV3(list_data.record_date).split("-");
-                if (itemYear === year) {
+                if (itemYear == year) {
                     accumulateData(result, list_data.asset_big_class, itemMonth, list_data.amount);
+                } else {
+                    console.log(list_data);
                 }
             });
 
