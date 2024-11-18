eca101d14997179049d4e46da0af79ba0fad5249
cmw1017 <cmw1017@naver.com>
2024-11-18 12:37:09 +0900

    241118-모바일 페이지 구현 시작

--- app/(pages)/asset_type/page.tsx
+++ app/(pages)/asset_type/page.tsx
@@ -2,15 +2,30 @@
 import Box from '@mui/material/Box';
 import AssetTypeTable from '@/components/asset_type/AssetTypeTable';
 import AssetTypeChart from '@/components/asset_type/AssetTypeChart';
+import { useMediaQuery } from "@mui/material";
 
 export default function AssetTypePage() {
 
+  // 모바일 페이지 체크
+  const isMobile = useMediaQuery('(max-width:600px) or (max-height:600px)');
+
   return (
     <Box sx={{ width: '100%' }}>
-      {/* AssetTypeTable 컴포넌트 */}
-      <AssetTypeTable />
-      {/* AssetTypeChart 컴포넌트 */}
-      <AssetTypeChart />
+      {isMobile ? (
+        <>
+          {/* AssetTypeTable 모바일 컴포넌트 */}
+          <AssetTypeTable />
+          {/* AssetTypeChart 모바일 컴포넌트 */}
+          <AssetTypeChart />
+        </>
+      ) : (
+        <>
+          {/* AssetTypeTable 컴포넌트 */}
+          <AssetTypeTable />
+          {/* AssetTypeChart 컴포넌트 */}
+          <AssetTypeChart />
+        </>
+      )}
     </Box>
   );
 }
